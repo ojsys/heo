830a5c09b5eb99ce817dd6fa99facbebf85bf331
Jonah Onah <onahjonah@gmail.com>
2025-11-20 20:58:34 +0100

    updated passenger

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,25 +1,29 @@
 import os
 import sys
 
+# cPanel Configuration - UPDATE THESE VALUES
+CPANEL_USERNAME = 'heoeziok'
+APP_NAME = 'heo'
+PYTHON_VERSION = '3.12'
+
 # Project directory
-project_home = os.path.dirname(os.path.abspath(__file__))
+project_home = f'/home/{CPANEL_USERNAME}/{APP_NAME}'
 
 # Add project directory to sys.path
 if project_home not in sys.path:
     sys.path.insert(0, project_home)
 
-# Virtual environment configuration
-# Change python_version to match your cPanel Python version (e.g., '3.9', '3.10', '3.11', '3.12')
-python_version = '3.12'
-venv_path = os.path.join(project_home, 'env', 'lib', f'python{python_version}', 'site-packages')
+# cPanel virtualenv paths
+# cPanel creates virtualenvs at: /home/username/virtualenv/appname/python_version/
+venv_base = f'/home/{CPANEL_USERNAME}/virtualenv/{APP_NAME}/{PYTHON_VERSION}'
+venv_paths = [
+    os.path.join(venv_base, 'lib', f'python{PYTHON_VERSION}', 'site-packages'),
+    os.path.join(venv_base, 'lib64', f'python{PYTHON_VERSION}', 'site-packages'),
+]
 
-if os.path.exists(venv_path):
-    sys.path.insert(0, venv_path)
-else:
-    # Try alternative path structure for some cPanel setups
-    alt_venv_path = os.path.join(project_home, 'env', 'lib64', f'python{python_version}', 'site-packages')
-    if os.path.exists(alt_venv_path):
-        sys.path.insert(0, alt_venv_path)
+for venv_path in venv_paths:
+    if os.path.exists(venv_path) and venv_path not in sys.path:
+        sys.path.insert(0, venv_path)
 
 # Load environment variables from .env file
 env_path = os.path.join(project_home, '.env')
